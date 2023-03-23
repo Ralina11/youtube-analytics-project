60219dbd6ecc6a911ac0e2edc17cdbf1d863a291
Ralina Latypova <Latypovaralina99@gmail.com>
2023-03-22 17:52:11 -0700

    add 2 hommework

--- src/channel.py
+++ src/channel.py
@@ -1,5 +1,5 @@
 from helper.youtube_api_manual import *
-
+from src.utils import *
 import json
 
 class Channel:
@@ -7,13 +7,51 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self.__channel_id, self.__title, self.__url, self.__subscriber_count, self.__video_count, self.__view_count = get_inf_dict(channel_id)
+    @property
+    def channel_id(self) -> str:
+        return self.__channel_id
+    @property
+    def title(self) -> str:
+        return self.__title
 
+    @property
+    def url(self) -> str:
+        return self.__url
+
+    @property
+    def subscriber_count(self) -> str:
+        return self.__subscriber_count
+
+    @property
+    def video_count(self) -> str:
+        return self.__video_count
+
+    @property
+    def view_count(self) -> str:
+        return self.__view_count
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
         print(json.dumps(channel, indent=2, ensure_ascii=False))
+
+    @staticmethod
+    def get_service() -> None:
+        """возвращающий объект для работы с YouTube API"""
+        return build('youtube', 'v3', developerKey=api_key)
+
+    def to_json(self,json_file):
+        data = {}
+        data['channel_id'] = self.__channel_id
+        data['title'] = self.__title
+        data['url'] = self.__url
+        data['subscriber_count'] = self.__subscriber_count
+        data['video_count'] = self.__video_count
+        data['view_count'] = self.__view_count
+
+        with open (json_file,'w') as outfile:
+            json.dump(data, outfile)
 
 
 
---
+++ src/utils.py
@@ -0,0 +1,19 @@
+from helper.youtube_api_manual import *
+from src.channel import *
+
+
+def get_inf_dict(channel_id: str):
+    """Возвращает информацию о канале in dictionaries"""
+    channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+    format_dict_info =  channel["items"][0]
+    title = format_dict_info["snippet"]["title"]
+    url = "https://www.youtube.com/channel/"+ channel_id
+    channel_id = format_dict_info["id"]
+    subscriberCount = format_dict_info["statistics"]["subscriberCount"]
+    videoCount = format_dict_info["statistics"]["videoCount"]
+    viewCount = format_dict_info["statistics"]["viewCount"]
+
+    return channel_id, title, url, subscriberCount, videoCount, viewCount
+
+
+
